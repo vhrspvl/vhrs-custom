e1ef45c8e55e6f2d7fc774ec544c1d3bcdb45af1
Abdulla P I <hereabdulla@gmail.com>
2017-10-04 14:59:24 +0530

    modified add_customer method to  save customer from portal

--- vhrs/utils.py
+++ vhrs/utils.py
@@ -10,11 +10,9 @@ from frappe.model.naming import make_autoname
 
 @frappe.whitelist()
 def add_customer(doc, method):
-    customer = frappe.db.get_value("User", {"email": doc.owner},
+    customer = frappe.db.get_value("User", {"email": frappe.session.user},
                                    ["customer"])
-    case = frappe.db.get_value("Cases", {"name": doc.name})
-    if case:
-        frappe.db.set_value("Cases", case, "customer", customer)
+    doc.customer = customer
 
 
 def addecode(doc, method):
