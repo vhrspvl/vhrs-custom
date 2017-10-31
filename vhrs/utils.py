dfa2ed7d5ba39a6ca0b3ec1c69933dd26d4fd532
Abdulla P I <hereabdulla@gmail.com>
2017-10-31 09:49:44 +0530

    Added Whitelisted method to check attendance API

--- vhrs/utils.py
+++ vhrs/utils.py
@@ -4,8 +4,10 @@
 
 from __future__ import unicode_literals
 import frappe
+import json
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname
+from frappe.utils.data import today
 
 
 @frappe.whitelist()
@@ -15,30 +17,54 @@ def add_customer(doc, method):
     doc.customer = customer
 
 
-# def update_status():
-#     projects = frappe.get_all('Project', fields={'name', 'status'})
-#     for project in projects:
-#         tasks = frappe.db.get_all('Task', fields={'name', 'project', 'status'}, filters={
-#             'project': project.name})
-#         if tasks:
-#             if all(task.status == 'Closed' or task.status == 'Completed' for task in tasks):
-#                 frappe.db.set_value(
-#                     "Project", project.name, "status", "Completed")
-#             elif all(task.status == 'Cancelled' for task in tasks):
-#                 frappe.db.set_value(
-#                     "Project", project.name, "status", "Cancelled")
-#             else:
-#                 frappe.db.set_value(
-#                     "Project", project.name, "status", "Open")
+def update_status(doc, method):
+    projects = frappe.get_all('Project', fields={'name', 'status'})
+    for project in projects:
+        tasks = frappe.db.get_all('Task', fields={'name', 'project', 'status'}, filters={
+            'project': project.name})
+        if tasks:
+            if any(task.status == 'Open' or task.status == 'Working' or task.status == 'Pending Review' or task.status == 'Overdue' for task in tasks):
+                frappe.db.set_value(
+                    "Project", project.name, "status", "Open")
+                frappe.errprint('hi')
+            elif all(task.status == 'Cancelled' for task in tasks):
+                frappe.db.set_value(
+                    "Project", project.name, "status", "Cancelled")
+                frappe.errprint('hii')
+            elif all(task.status == 'DnD' for task in tasks):
+                frappe.db.set_value(
+                    "Project", project.name, "status", "Completed")
+                frappe.errprint('hiii')
 
-#     customers = frappe.get_all('Customer', fields={'name', 'status'})
-#     for customer in customers:
-#         projects = frappe.db.get_all('Project', fields={'name', 'customer', 'status'}, filters={
-#             'customer': customer.name})
-#         if projects:
-#             if all(project.status == 'Open' for project in projects):
-#                 frappe.db.set_value(
-#                     "Customer", customer.name, "status", "Open")
-#             else:
-#                 frappe.db.set_value(
-#                     "Customer", customer.name, "status", "Active")
+    customers = frappe.get_all('Customer', fields={'name', 'status'})
+    for customer in customers:
+        projects = frappe.db.get_all('Project', fields={'name', 'customer', 'status'}, filters={
+            'customer': customer.name})
+        if projects:
+            if all(project.status == 'Open' for project in projects):
+                frappe.db.set_value(
+                    "Customer", customer.name, "status", "Open")
+            else:
+                frappe.db.set_value(
+                    "Customer", customer.name, "status", "Active")
+
+
+@frappe.whitelist(allow_guest=False)
+def attendance(**args):
+    args = frappe._dict(args)
+    attendance_id = frappe.db.get_value("Attendance", {
+        "employee": args.userid, "attendance_date": today()})
+    if attendance_id:
+        attendance = frappe.get_doc("Attendance", attendance_id)
+        frappe.errprint(attendance.attendance_date)
+    else:
+        name, company = frappe.db.get_value(
+            "Employee", args.userid, ["employee_name", "company"])
+        attendance = frappe.new_doc("Attendance")
+        attendance.employee = args.userid
+        attendance.employee_name = name
+        attendance.attendance_date = "30-10-2017"
+        attendance.status = "Present"
+        attendance.company = company
+        frappe.errprint(attendance.employee_name)
+    return 'ok'
